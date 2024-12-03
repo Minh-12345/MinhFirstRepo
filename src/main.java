c77da3ce180e8a875cb7a0c31a9b6a02e374929a
Minh-12345 <minh.doduy.cit22@eiu.edu.vn>
2024-12-03 08:55:20 +0700

    Update Main

--- src/main.java
+++ src/main.java
@@ -1,5 +1,5 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Minh's Java Repository.");
+        System.out.println("Hello Minh.\nWelcome to my repository");
     }
 }
